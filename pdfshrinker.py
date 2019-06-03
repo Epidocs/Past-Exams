54002310dc0946847462d1b002eaaf9fd976c484
Matiboux <matiboux@gmail.com>
2019-06-03 19:38:44 +0200

    Updated pdfshrinker.py to run on Windows

--- pdfshrinker.py
+++ pdfshrinker.py
@@ -1,15 +1,21 @@
 import os
-import sys
-import subprocess
+from sys import argv
+from shutil import which
+from subprocess import run
+
+walkpath = argv[1] if len(argv) >= 2 else '.'
+
+if which('gswin64c') is not None: gs = 'gswin64c' # Windows 64-bit
+elif which('gswin32c') is not None: gs = 'gswin32c' # Windows 32-bit
+else: gs = 'gs'
 
 def shrink_file(root, filename):
 	f, e = os.path.splitext(filename) 
 	if e == '.pdf':
-		print(root)
 		path = os.path.join(root, filename)
 		npath = os.path.join(root, f + '_shrink' + e)
 		
-		subprocess.run(['gs', '-q', '-dNOPAUSE', '-dBATCH', '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/ebook' ,'-sOutputFile=' + npath, path])
+		run([gs, '-q', '-dNOPAUSE', '-dBATCH', '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/ebook' ,'-sOutputFile=' + npath, path])
 		if os.path.isfile(npath):
 			if os.path.getsize(npath) < os.path.getsize(path):
 				# The new file is smaller than the existing one; keep it
@@ -18,8 +24,6 @@ def shrink_file(root, filename):
 			else:
 				# The new file is larger than the existing one; delete it
 				os.remove(npath)
-
-walkpath = sys.argv[1] if len(sys.argv) >= 2 else '.'
 
 if os.path.isfile(walkpath):
 	root, filename = os.path.split(walkpath)
