bece7ff11672afde54fb103ab2c879d6aad79d01
Matiboux <matiboux@gmail.com>
2019-11-14 02:25:48 +0100

    Fixed deploy.php

--- deploy.php
+++ deploy.php
@@ -51,7 +51,7 @@ function getMetaInfos($pathList) {
 				$metaInfos[$i] = $metaJSONs[$dir]['folder'];
 			
 			// Check if the subfolders listing is allowed
-			if(isset($metaJSONs[$dir]['allow_subfolders_listing']) AND $metaInfos[$i]['allow_subfolders_listing'])) {
+			if(isset($metaJSONs[$dir]['allow_subfolders_listing']) AND $metaJSONs[$dir]['allow_subfolders_listing'])) {
 				// Check if no subfiles in this folder
 				$subfiles = glob($path . '/*.*');
 				if(empty($subfiles)) {
@@ -61,7 +61,7 @@ function getMetaInfos($pathList) {
 					foreach($subfolders as $folder) {
 						$metaInfos[$i]['subfolders'][] = array(
 							'basename' => basename($folder),
-							'path' =>str_replace('#', '%23', $folder)
+							'path' => str_replace('#', '%23', $folder)
 						);
 					}
 				}
