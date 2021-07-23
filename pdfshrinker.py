2841e7995c6c6040f5a62dd6d0ff1faf6f0f1760
Binary <ewan@barel.org>
2021-07-23 12:50:16 +0200

    pdfshrinker: added some logging
    + added some logging in order to display the file currently being
    processed and its reduction rate

--- pdfshrinker.py
+++ pdfshrinker.py
@@ -11,20 +11,30 @@ elif which('gs') is not None: gs = 'gs' # Other
 else: sys.exit('Error: Could not find GhostScript!')
 
 def shrink_file(root, filename):
-	f, e = os.path.splitext(filename) 
+	f, e = os.path.splitext(filename)
 	if e == '.pdf':
 		path = os.path.join(root, filename)
 		npath = os.path.join(root, f + '_shrink' + e)
-		
+
 		run([gs, '-q', '-dNOPAUSE', '-dBATCH', '-sDEVICE=pdfwrite', '-dPDFSETTINGS=/ebook' ,'-sOutputFile=' + npath, path])
 		if os.path.isfile(npath):
-			if os.path.getsize(npath) < os.path.getsize(path):
+			size_pre = os.path.getsize(path) # File size before shrinking
+			size_post = os.path.getsize(npath) # File size after shrinking
+
+			reduce_rate, reduce_bytes = (0.0, 0)
+
+			if size_post < size_pre:
 				# The new file is smaller than the existing one; keep it
 				os.remove(path)
 				os.rename(npath, path)
+
+				reduce_rate = 1.0 - size_post / size_pre
+				reduce_bytes = size_pre - size_post
 			else:
 				# The new file is larger than the existing one; delete it
 				os.remove(npath)
+
+			print("shrink_file: {:.2f} % -- {} ({:,} bytes)".format(-reduce_rate * 100, path, -reduce_bytes))
 
 if os.path.isfile(walkpath):
 	root, filename = os.path.split(walkpath)
